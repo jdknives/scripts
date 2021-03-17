27eeef508d197c62fc3a17ad22c94d4ceba00021
Erich Kaestner <36450093+jdknives@users.noreply.github.com>
2021-03-16 22:41:41 -0300

    fix script

--- git/add_remote.sh
+++ git/add_remote.sh
@@ -7,14 +7,20 @@ DIR=$(basename $(PWD))
 if [ $DIR = "dmsg" ]; then
   git remote add darkren https://github.com/Darkren/dmsg.git
   git remote add evan https://github.com/evanlinjin/dmsg.git
-  git remote add nikita https://github.com/nkryuchkov/dmsg.git
+  git remote add frank https://github.com/i-hate-nicknames/dmsg.git
+  git remote add rudi https://github.com/jdknives/dmsg.git
   git remote update
 elif [ $DIR = "skywire" ]; then
   git remote add darkren https://github.com/Darkren/skywire.git
   git remote add evan https://github.com/evanlinjin/skywire.git
-  git remote add nikita https://github.com/nkryuchkov/skywire.git
   git remote add senyoret https://github.com/Senyoret1/skywire-mainnet.git
-  git remote add taras https://github.com/taras-skycoin/skywire.git
+  git remote add frank https://github.com/i-hate-nicknames/skywire.git
+  git remote add rudi https://github.com/jdknives/skywire.git
+  git remote update
+elif [ $DIR = "skybian" ]; then
+  git remote add rudi https://github.com/jdknives/skybian.git
+  git remote add frank https://github.com/i-hate-nicknames/skybian.git
+  git remote add asxtree https://github.com/asxtree/skybian.git
   git remote update
 else
       echo "Did not recognize the repo."
