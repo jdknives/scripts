3248e5ef54308b471ce04cde10dfeae95d04d96e
jdknives <jdknives@users.noreply.github.com>
2021-05-30 22:12:01 -0400

    add remote-update script

---
+++ skywire/update-remotes.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#This script checks if the current directory is a git repository.
+#If it is, it grabs all remotes of developers with open PR to the repo
+#and adds them as remotes. 
+
+current_dir=${PWD##*/}
+repo_pulls_url="https://github.com/skycoin/$current_dir/pulls"
+
+IFS="
+"
+
+if ! git rev-parse --git-dir > /dev/null 2>&1; then
+    echo "Not inside a git repo. Aborting."
+    exit
+fi
+
+devsArray=($(curl $repo_pulls_url | grep -Po '(?<=created\sby\s)[^"]*' ))
+
+devsArray=( `for i in ${devsArray[@]}; do echo $i; done | sort -u` )
+
+remoteArray=($(git remote))
+
+for i in "${devsArray[@]}"
+do
+    if [[ ! " ${remoteArray[@]} " =~ " ${i} " ]]; then
+    git remote add $i "https://github.com/$i/$current_dir.git"
+    fi
+done
+
+git remote update
